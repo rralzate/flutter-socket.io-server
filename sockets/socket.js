f4929e71b2fe1554ed4d5a4a27d416cecb25eaa9
Ricardo Reyes Alzate <rreyes@todo1.net>
2022-01-08 21:55:03 -0500

    Cambios sobre bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,23 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand( new Band( 'Qeen' ));
+bands.addBand( new Band( 'Bon Jovi' ));
+bands.addBand( new Band( 'Metallica' ));
+bands.addBand( new Band( 'Soda estereo' ));
+
+console.log(bands);
 
 // Mensajes de Sockets
 io.on('connection', client => {   
     
     console.log('Cliente conectado');
+
+    client.emit('bandas-activas', bands.getBands())
+
     client.on('disconnect', () => { 
         console.log('Cliente desconectado'); 
     });
@@ -12,5 +26,40 @@ io.on('connection', client => {
         console.log('mensaje!!!', payload);
 
         io.emit('mensaje', {admin: 'Nuevo mensaje' });
-    })
+    });
+
+    client.on('vote-band', (payload) => {
+
+        console.log(payload);
+        bands.voteBand(payload.id);
+        io.emit('bandas-activas', bands.getBands());
+       // client.broadcast.emit('nuevo-mensaje', payload); //Emite a todos menos al cliente
+
+    });
+
+    client.on('add-band',  ( payload ) => {   
+        
+        const newBand = new Band( payload.name );
+        bands.addBand(newBand);
+        io.emit('bandas-activas', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        
+        bands.deleteBand(payload.id);
+        io.emit('bandas-activas', bands.getBands());       
+
+    });
+    
+
+
+    // client.on('emitir-mensaje', (payload) => {
+
+    //     console.log(payload);
+    //    // io.emit('nuevo-mensaje', payload);
+    //     client.broadcast.emit('nuevo-mensaje', payload); //Emite a todos menos al cliente
+
+    // });
+
+
   });
